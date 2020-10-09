a0932db2f26f1fba65d101e02ef08e7271ae2fc9
Stefan Lukovic <mekbuk@Stefans-MacBook-Pro.local>
2020-10-09 14:40:43 +0200

    init commit

---
+++ android/app/src/main/kotlin/com/example/basketindex/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.basketindex
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/score_input.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class ScoreInput extends StatefulWidget {
+  @override
+  _ScoreInputState createState() => _ScoreInputState();
+}
+
+class _ScoreInputState extends State<ScoreInput> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
